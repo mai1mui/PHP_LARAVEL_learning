2fd776c418b3673a305e17796a7f327b1fa44d6c
Hạnh Mai <hanhmai120997@gmail.com>
2025-09-01 23:43:27 +0700

    Create ex02_intro.php

---
+++ Lesson01_intro-in-w3school/ex02_intro.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Example</title>
+</head>
+<body>
+    <?php echo "Hello, students!"; ?> 
+    //Đây là thẻ mở và đóng của mã PHP trong tài liệu HTML.
+    //Lệnh echo dùng để xuất nội dung ra màn hình.
+</body>
+</html>
