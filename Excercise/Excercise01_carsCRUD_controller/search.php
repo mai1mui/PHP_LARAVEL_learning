b525fb6e8c5541816d2e7b16e9581b03d67b8370
Hạnh Mai <hanhmai120997@gmail.com>
2025-09-02 01:03:21 +0700

    Delete Excercise/Excercise01_carsCRUD_controller directory

--- Excercise/Excercise01_carsCRUD_controller/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-#1.ket noi controller
-include './CarController.php';
-#2.doc du lieu
-if($_SERVER['SERVER_METHOD']=="POST"):
-    $keyword-$_POST['txtSearch'];
-    $fields=$carbrand->search($keyword);
-endif;
-?>
