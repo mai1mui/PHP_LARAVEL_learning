bb39a17c50e65ace55650dbf9c5ff14101e64321
Hạnh Mai <hanhmai120997@gmail.com>
2025-09-01 23:42:30 +0700

    Add files via upload

---
+++ Lesson01_Intro/Ex02_A-WebPage.php
@@ -0,0 +1,21 @@
+<?php  
+    #Code PHP ở đây 
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>First PHP WebPage</title>
+        <style> 
+            span{ 
+                color: #808080;
+                font-style: italic;
+                font-size: 20pt;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Mum, here is My first PHP WebPage</h1>
+        <span>I want to cry!</span>
+    </body>
+</html>
